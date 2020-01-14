0a879af96b0b979df9cfea5ea4c17512669b668a
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-01-14 15:48:18 +0100

    test tidied

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const monolith = require('./')
+const sha256 = require('./')
 const crypto = require('crypto')
 const ref = require('js-sha256')
 const sodium = require('sodium-native')
@@ -8,16 +8,16 @@ const sodium = require('sodium-native')
   const buf = Buffer.alloc(8192)
   sodium.randombytes_buf(buf)
 
-  const monoHash = monolith()
+  const hash = sha256()
   const jsHash = ref.create()
   const refHash = crypto.createHash('sha256') 
   
-  console.time('monolith')
+  console.time('wasm')
   for (let i = 0; i < 1000; i++) {
-    monoHash.update(buf)
+    hash.update(buf)
   }
-  const monoRes = monoHash.digest('hex')
-  console.timeEnd('monolith')
+  const res = hash.digest('hex')
+  console.timeEnd('wasm')
 
   console.time('js')
   for (let i = 0; i < 1000; i++) {
@@ -33,7 +33,7 @@ const sodium = require('sodium-native')
   const refRes = refHash.digest('hex')
   console.timeEnd('native')
 
-  console.log('\nhashes are consistent: ', monoRes === refRes && monoRes === jsRes)
+  console.log('\nhashes are consistent: ', res === refRes && res === jsRes)
 }
 
 // naive input fuzz
@@ -43,7 +43,7 @@ for (let i = 0; i < 100; i++) {
   const length = Math.floor(2 ** 18 * Math.random())
   const buf = Buffer.alloc(length)
   sodium.randombytes_buf(buf)
-  const hash = monolith().update(buf).digest('hex')
+  const hash = sha256().update(buf).digest('hex')
   const ref = crypto.createHash('sha256').update(buf).digest('hex')
 
   if (hash !== ref) bugs.push(length)
@@ -52,16 +52,16 @@ for (let i = 0; i < 100; i++) {
 console.log('\nhashes inconsistent at lengths:', bugs, '\n')
 
 // fuzz multiple updates
-const monoHash = monolith()
+const hash = sha256()
 const refHash = crypto.createHash('sha256') 
 
 for (let i = 0; i < 100; i++) {
   const buf = Buffer.alloc(2**16 * Math.random())
   sodium.randombytes_buf(buf)
   
-  monoHash.update(buf)
+  hash.update(buf)
   refHash.update(buf)
 }
 
-console.log(monoHash.digest('hex'))
+console.log(hash.digest('hex'))
 console.log(refHash.digest('hex'))
